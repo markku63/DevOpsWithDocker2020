aeeaf324d416137f92ba3f471ab1dba5a5a2d138
Markku Kolkka <markku.kolkka@iki.fi>
2020-08-25 11:12:38 +0300

    Exercise 3.3

---
+++ Part3/3/my_builder.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+if [ $# -ne 1 ]
+then
+    echo "Download, build and publish a GitHub repo as a Docker image"
+    echo "Usage:"
+    echo "$0 <repository URL>"
+    echo "Where <repository URL> is an URL to a GitHub repository with a Dockerfile"
+    exit 64
+fi
+BUILD_DIR=$(mktemp -d .buildXXXXXXX)
+git clone --depth 1 --recurse-submodules --shallow-submodules $1 $BUILD_DIR
+pushd $BUILD_DIR
+if [ -e 'Dockerfile' ]
+then
+    docker build .
+else
+    echo "Not a Docker project!"
+    exit 1
+fi
+popd
+rm -rf $BUILD_DIR
